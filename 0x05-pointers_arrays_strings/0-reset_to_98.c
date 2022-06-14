b2bbf5971ca47b2c464a40bebca45090079e3b05
WaburiBK <bwaburi007@gmail.com>
2022-06-14 11:18:06 -0700

    update to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * reset_to_98 - function that takes a pointer and updates its value to 98
- *   * Description: Program that resets to 98
- *    * @n: the interger to reset to
- *     *
- *      */
+*reset_to_98 - function that takes a pointer and updates its value to 98
+*Description: Program that resets to 98
+*@n: the interger to reset to
+*/
 void reset_to_98(int *n)
 {
-		*n = 98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++
@@ -1,8 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/* _putchar */
-void _putchar(char n);
-
-/* 0.98 Battery Street */
-void reset_to_98(int n);
