ea388a6c48aff93a8a45bcd4e774610e42989a10
WaburiBK <bwaburi007@gmail.com>
2022-06-16 12:33:56 -0700

    reverse array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,79 @@
 #include "main.h"
 
 /**
-* reverse_array -> reversing an array
-* @a: array a
-* @n: an element of an array
-*/
-void reverse_array(int *a, int n)
-{
-	int *p, i, aux, k;
+ *
+ *  * reverse_array - reverses the content of an array of integers
+ *
+ *   * @a: an array of integers
+ *
+ *    * @n: number of elements of the array
+ *
+ *     * Return: returns void
+ *
+ *      */
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
+
+
+void reverse_array(int *a, int n)
+
+
+
+{
+
+	int *start_c, *end_c, c;
+
+	int i;
+
+
+
+
+
+	start_c = a;
+
+	end_c = a;
+
+
+
+
+
+	for (i = 0; i < n - 1; i++)
+
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+
+		end_c++;
+
 	}
+
+
+
+
+
+
+
+
+
+	for (i = 0; i < n / 2; i++)
+
+	{
+
+
+
+
+
+		c = *end_c;
+
+		*end_c = *start_c;
+
+		*start_c = c;
+
+
+
+		start_c++;
+
+		end_c--;
+
+	}
+
+
+
 }
