2616b564e1f79384546cb68288594507c9f421a8
WaburiBK <bwaburi007@gmail.com>
2022-07-21 09:23:15 -0700

    convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,37 +1,65 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
+ * expo - take base and raise to a power
+ * @power: exponent
+ * @base: base to raise
  *
- * Return: unsigned int.
+ * Return: unsigned int answer
+ */
+unsigned int expo(unsigned int power, unsigned int base)
+{
+	unsigned int answer = 1;
+
+	while (power != 0)
+	{
+		answer *= base;
+		power--;
+	}
+
+	if (power == 0)
+		answer *= 1;
+
+	return (answer);
+}
+
+/**
+ * binary_to_uint - converty binary string to unsigned int
+ * @b: binary number as a string
+ *
+ * Return: number if successfull, 0 if input is NULL or contains a letter
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int total = 0;
+	unsigned int mult, power;
+	unsigned int base = 2;
+	size_t i, j;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	ui = 0;
+	/* length minus 1 will be the power */
+	j = 0;
+	while (b[j])
+		j++;
+	power = j - 1;
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	i = 0;
+	/* loop through binary as string */
+	while (b[i] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		/* check for non binary characters */
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
 
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		mult = ((b[i] == '1') ? 1 : 0);
+
+		total +=  mult * (expo(power, base));
+		power--;
+		i++;
 	}
 
-	return (ui);
+	return (total);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
+#include <stddef.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
